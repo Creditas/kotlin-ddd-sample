6b218c09a625353a43e2139ff939c01d381edc50
fabriciorissetto <fabriciorissetto@gmail.com>
2018-07-03 14:16:05 -0300

    wip

--- web/src/main/SpringBootApplication.kt
+++ web/src/main/SpringBootApplication.kt
@@ -1,11 +1,11 @@
 package kotlinddd.web
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.boot.SpringApplication
+//import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication
-class Application
+//@SpringBootApplication
+//class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    //SpringApplication.run(Application::class.java, *args)
 }
--- web/src/main/module-info.java
+++ web/src/main/module-info.java
@@ -1,12 +1,13 @@
 module kotlinddd.web {
-    exports kotlinddd.web.controllers;
+    opens kotlinddd.web;
+    //exports kotlinddd.web.controllers;
 
-    opens kotlinddd.web to spring.core, spring.beans, spring.context;
+    //opens kotlinddd.web to spring.core, spring.beans, spring.context;
 
-    requires spring.core;
     requires kotlin.stdlib;
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
-    requires spring.web;
-    requires java.sql;
+//    requires spring.core;
+//    requires spring.boot;
+//    requires spring.boot.autoconfigure;
+//    requires spring.web;
+//    requires java.sql;
 }
