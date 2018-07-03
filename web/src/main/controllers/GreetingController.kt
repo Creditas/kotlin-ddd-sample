4db79d468a646135024d551c7d4987c88e103b92
fabriciorissetto <fabriciorissetto@gmail.com>
2018-07-03 14:44:03 -0300

    Cleaning idea cache and fixing the build

--- web/src/main/controllers/GreetingController.kt
+++ web/src/main/controllers/GreetingController.kt
@@ -1,19 +1,19 @@
 package kotlinddd.web.controllers
 
-//import kotlinddd.web.models.Greeting
-//import org.springframework.web.bind.annotation.GetMapping
-//import org.springframework.web.bind.annotation.PostMapping
-//import org.springframework.web.bind.annotation.RequestParam
-//import org.springframework.web.bind.annotation.RestController
-//
-//@RestController
-//class GreetingController {
-//
-//    @PostMapping("/order")
-//    fun createOrder(@RequestParam(value = "name", defaultValue = "World") name: String) =
-//            Greeting(1, "Hello, $name")
-//
-//    @GetMapping("/test")
-//    fun test(@RequestParam(value = "name", defaultValue = "World") name: String) =
-//            "Hello, $name"
-//}
+import kotlinddd.web.models.Greeting
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingController {
+
+    @PostMapping("/order")
+    fun createOrder(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(1, "Hello, $name")
+
+    @GetMapping("/test")
+    fun test(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            "Hellooooooo, $name"
+}
--- web/src/main/module-info.java
+++ web/src/main/module-info.java
@@ -1,13 +1,12 @@
 module kotlinddd.web {
-    opens kotlinddd.web;
-    //exports kotlinddd.web.controllers;
+    exports kotlinddd.web.controllers;
 
-    //opens kotlinddd.web to spring.core, spring.beans, spring.context;
+    opens kotlinddd.web to spring.core, spring.beans, spring.context;
 
     requires kotlin.stdlib;
-//    requires spring.core;
-//    requires spring.boot;
-//    requires spring.boot.autoconfigure;
-//    requires spring.web;
-//    requires java.sql;
+    requires spring.core;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+    requires java.sql;
 }
