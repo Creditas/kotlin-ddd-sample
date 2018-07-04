7f8975e8f2510abaab3117838ed6d4157e4da470
fabriciorissetto <fabriciorissetto@gmail.com>
2018-07-04 10:28:25 -0300

    Ignoring idea files and renaming modules files to txt

--- web/src/main/SpringBootApplication.kt
+++ web/src/main/SpringBootApplication.kt
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-    //kotlinddd.application.registerHandlers()
+    kotlinddd.application.registerHandlers()
     SpringApplication.run(Application::class.java, *args)
 }
