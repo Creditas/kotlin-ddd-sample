252f40355cc5c25b004a0e0dcc4cfb81a24bf528
fabriciorissetto <fabriciorissetto@gmail.com>
2018-07-05 11:38:00 -0300

    Implementing domain layer

--- domain/src/main/order/Order.kt
+++ domain/src/main/order/Order.kt
@@ -1,6 +1,36 @@
-//package kotlinddd.domain.order
-//
-//class Order {
-//    //  @Id
-//    lateinit var id: String;
-//}
+package kotlinddd.domain.order
+
+import kotlinddd.domain.BusinessException
+import order.Customer
+import java.util.UUID
+
+class Order(val id: UUID, val customer: Customer) {
+    private val items = mutableListOf<Item>()
+
+    fun addProduct(product: Product, quantity: Int) {
+        if (items.any { it.product == product })
+            throw BusinessException("Product already exists!")
+
+        var item = Item(product, quantity)
+        items.add(item)
+    }
+
+    fun changeProductQuantity(product: Product, quantity: Int) {
+        validadeIfProductIsOnList(product)
+
+        var item = items.first { it.product == product }
+        item.changeQuantity(quantity)
+    }
+
+    fun removeProduct(product: Product, quantity: Int) {
+        validadeIfProductIsOnList(product)
+
+        items.removeAll { it.product == product }
+    }
+
+    fun validadeIfProductIsOnList(product: Product) {
+        var isOnList = items.any { it.product == product }
+        if (!isOnList)
+            throw BusinessException("The product isn't included in this order")
+    }
+}
