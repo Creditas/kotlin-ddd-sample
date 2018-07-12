dfabe1a4608b569db34371c29c98d52fa233ed8f
fabriciorissetto <fabriciorissetto@gmail.com>
2018-07-12 15:03:46 -0300

    Configuring rabbit processors

--- web/src/main/configuration/injection/AMQP.kt
+++ web/src/main/configuration/injection/AMQP.kt
@@ -1,7 +1,5 @@
 package kotlinddd.web.configuration.injection
 
-import org.axonframework.amqp.eventhandling.spring.SpringAMQPPublisher
-import org.axonframework.eventhandling.EventBus
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,47 +9,23 @@ import org.axonframework.serialization.Serializer
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.core.Message
 import org.axonframework.config.EventHandlingConfiguration
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
-import org.springframework.amqp.rabbit.connection.ConnectionFactory
-import org.springframework.beans.factory.annotation.Value
+import org.axonframework.config.DefaultConfigurer
+import org.axonframework.config.EventProcessingConfiguration
+
 
 @Configuration
-class AMQPReader {
+class AMQP {
     @Autowired
-    lateinit var eventBus: EventBus
+    lateinit var springAMQPMessageSource: SpringAMQPMessageSource
 
-    @Value("\${spring.rabbitmq.host}")
-    private lateinit var rabbitHost: String
-
-    @Value("\${spring.rabbitmq.port}")
-    private var rabbitPort: Int = 0
-
-    @Value("\${spring.rabbitmq.username}")
-    private lateinit var rabbitUserName: String
-
-    @Value("\${spring.rabbitmq.password}")
-    private lateinit var rabbitPassword: String
-
-    @Autowired
-    fun configure(ehConfig: EventHandlingConfiguration, rabbitMessageSource: SpringAMQPMessageSource) {
-        ehConfig.registerSubscribingEventProcessor("rabbitSubscriberProcessor") { _ -> rabbitMessageSource }
-    }
+//    @Autowired
+//    fun configure(configurer: DefaultConfigurer, eventProcessingConfiguration: EventProcessingConfiguration, eventHandlingConfiguration: EventHandlingConfiguration, rabbitMessageSource: SpringAMQPMessageSource) {
+//        //TODO: find a way to make the springAMQPMessageSource the default source for all processors
+//        eventProcessingConfiguration.registerSubscribingEventProcessor("kotlinddd.application.shipping.eventhandlers") { _ -> springAMQPMessageSource }
+//    }
 
     @Bean
-    fun getSpringAMQPPublisher() : SpringAMQPPublisher {
-        return SpringAMQPPublisher(eventBus)
-    }
-
-    @Bean
-    fun getConnectionFactory(): ConnectionFactory {
-        val connectionFactory = CachingConnectionFactory(rabbitHost, rabbitPort)
-        connectionFactory.setUsername(rabbitUserName)
-        connectionFactory.setPassword(rabbitPassword)
-        return connectionFactory
-    }
-
-    @Bean
-    fun rabbitMessageSource(serializer: Serializer): SpringAMQPMessageSource {
+    fun myRabbitMessageSource(serializer: Serializer): SpringAMQPMessageSource {
         return object : SpringAMQPMessageSource(serializer) {
             @RabbitListener(queues = ["DomainEvents"])
             @Throws(Exception::class)
